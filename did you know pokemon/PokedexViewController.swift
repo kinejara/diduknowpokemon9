a544b56fcf82a1913ed6883e6f5e5f79fa861e56
Jorge Villanueva <jorge@gopangea.com>
2016-03-12 23:19:10 -0600

    includes basic connection to UITableView

--- did you know pokemon/PokedexViewController.swift
+++ did you know pokemon/PokedexViewController.swift
@@ -8,6 +8,31 @@
 
 import UIKit
 
-class PokedexViewController: UIViewController {
-
+class PokedexViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    let facts : [String] = ["fact1", "fact2"]
+    let cellId = "pokeCell"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: self.cellId)
+    }
+    
+    // MARK: - Table view data source
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.facts.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier(cellId)! as UITableViewCell
+        cell.textLabel?.text = self.facts[indexPath.row]
+        
+        return cell
+    }
 }
