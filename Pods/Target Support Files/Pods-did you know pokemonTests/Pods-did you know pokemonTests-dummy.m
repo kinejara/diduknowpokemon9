d48bf7b41bb805e92a5b29d1851e2b19928e36b8
Jorge kinejara <ge0@Jorges-MacBook.local>
2016-03-26 21:13:07 -0500

    adpat to new mac

--- Pods/Target Support Files/Pods-did you know pokemonTests/Pods-did you know pokemonTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_did_you_know_pokemonTests : NSObject
-@end
-@implementation PodsDummy_Pods_did_you_know_pokemonTests
-@end
--- Pods/Target Support Files/Pods-did you know pokemonUITests/Pods-did you know pokemonUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_did_you_know_pokemonUITests : NSObject
-@end
-@implementation PodsDummy_Pods_did_you_know_pokemonUITests
-@end
