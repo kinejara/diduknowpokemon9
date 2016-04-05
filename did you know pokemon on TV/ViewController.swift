1609619f130ea0e9a81d1352e7768dea63a13f0b
Jorge Villanueva <jorge@gopangea.com>
2016-04-05 00:50:27 -0500

    includes auto-layout for apple tv

--- did you know pokemon on TV/ViewController.swift
+++ did you know pokemon on TV/ViewController.swift
@@ -8,18 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    var facts : [String] = PkmnFacts().allFacts
+    let cellId = "pokeCell"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.tableView.backgroundColor = UIColor.appBackgroundColor()
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: self.cellId)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.facts.count
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+    
+    func tableView(tableView: UITableView, estimatedHeightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier(cellId)! as UITableViewCell
+        cell.textLabel?.font = UIFont(name: "PokemonGB", size: 34.0)
+        cell.textLabel?.numberOfLines = 0
+        cell.textLabel?.textAlignment = NSTextAlignment.Center
+        cell.textLabel?.text = "\n" + self.facts[indexPath.row] + "\n"
+        
+        cell.contentView.backgroundColor = UIColor.clearColor()
+        cell.backgroundColor = UIColor.clearColor()
+        
+        return cell
+    }
 
 }
 
