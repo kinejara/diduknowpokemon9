364af84abec317b1645e623f8426d73422adcabc
Jorge Villanueva <jorge@gopangea.com>
2016-04-05 00:09:01 -0500

    includes apple tv project

---
+++ did you know pokemon on TVTests/did_you_know_pokemon_on_TVTests.swift
@@ -0,0 +1,36 @@
+//
+//  did_you_know_pokemon_on_TVTests.swift
+//  did you know pokemon on TVTests
+//
+//  Created by Jorge Villa on 4/5/16.
+//  Copyright © 2016 kine. All rights reserved.
+//
+
+import XCTest
+@testable import did_you_know_pokemon_on_TV
+
+class did_you_know_pokemon_on_TVTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
